2ed811c694d0108848565902e94e161479f2e2d5
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-03-28 14:41:30 +0100

    1.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,100 @@
-const Header = (props) =>{
+import { useState } from 'react'
+
+const Button = ({text, activated}) => {
+  
+  return(
+    <button onClick={() => activated(text)}>{text}</button>
+  )
+}
+const Buttons = ({activated}) => {
+  return(
+    <>
+      <Button text = {"good"} activated = {activated}/>
+      <Button text = {"neutral"} activated = {activated}/>
+      <Button text = {"bad"} activated = {activated}/>
+    </>
+  )
+}
+
+const StaticLine = ({text,value}) =>{
 
   return(
-    <>
-      <h1>{props.title.name}</h1>
-    </>
-  )
-}
-const Content = (props) =>{
-  return(
-    <>
-      <Part case = {props.group.parts[0]} />
-      <Part case = {props.group.parts[1]} />
-      <Part case = {props.group.parts[2]} />
-    </>
-  )
-}
-const Part = (props) =>{
-  return(
-    <>
-      <p>{props.case.name} {props.case.exercises}</p>
-    </>
-  )
-}
-const Total = (props) =>{
-
-  return(
-    <>
-      <p>Number of exercises {props.group.parts[0].exercises + props.group.parts[1].exercises + props.group.parts[2].exercises} </p>
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 
 }
+
+const Display = ({results}) => {
+  const [good, neutral, bad] = results
+
+  const all = good + neutral + bad
+
+  if(all == 0){
+    return(
+      <>
+        <h3>No feedback give</h3>
+      </>
+    )
+  }
+  const average = all / 3
+  const positive = (good * 100) / all
+
+  return(
+    <table>
+      <tbody>
+
+        <StaticLine text={"Good"} value={good} />
+        <StaticLine text={"Neutral"} value={neutral} />
+        <StaticLine text={"Bad"} value={bad} />
+
+        <StaticLine text={"All"} value={all} />
+        <StaticLine text={"Average"} value={average} />
+        <StaticLine text={"Positive"} value={positive} />
+
+      </tbody>
+
+
+    </table>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const activated = (action) =>{
+    switch (action){
+      case "good":
+        setGood(good +1)
+        break
+
+      case "neutral":
+        setNeutral(neutral +1)
+        break
+
+      case "bad":
+        setBad(bad +1)
+        break
+
+    }
   }
   return (
     <>
-      <Header title = {course}/>
-      
-      <Content group = {course} />
-      
-      <Total group = {course} />
+      <h2>Give feedback</h2>
+
+      <Buttons activated={activated}/>
+
+      <h2>Statistics</h2>
+
+      <Display results = {[good, neutral, bad]}/>
     </>
   )
 }
+
 
 export default App
